058c11605dabb24616d6347edf364ddca0d9d38d
JSJeong <hy6219@naver.com>
2022-01-04 18:06:09 +0900

    docs:JPA 실습부분

---
+++ ch02-jpa-start1/src/main/java/com/example/ch02jpastart1/Ch02JpaStart1Application.java
@@ -0,0 +1,13 @@
+package com.example.ch02jpastart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch02JpaStart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch02JpaStart1Application.class, args);
+    }
+
+}
