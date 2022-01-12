279ba41b0dbeb55b3407c68fd8c1d858952cb383
JSJeong <hy6219@naver.com>
2022-01-12 09:32:11 +0900

    chore:@Enumerated 실습용 세팅

---
+++ ch04-jpa-start3/src/main/java/com/example/ch04jpastart3/Ch04JpaStart3Application.java
@@ -0,0 +1,13 @@
+package com.example.ch04jpastart3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch04JpaStart3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch04JpaStart3Application.class, args);
+    }
+
+}
---
+++ ch04-jpa-start3/src/test/java/com/example/ch04jpastart3/Ch04JpaStart3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch04jpastart3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch04JpaStart3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
