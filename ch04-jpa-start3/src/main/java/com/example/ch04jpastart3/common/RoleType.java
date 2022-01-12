8089e38bf737024fa09d3d2e3e2f48972fdde45f
JSJeong <hy6219@naver.com>
2022-01-12 10:11:18 +0900

    chore: enum 일부 변경

--- ch04-jpa-start3/src/main/java/com/example/ch04jpastart3/common/RoleType.java
+++ ch04-jpa-start3/src/main/java/com/example/ch04jpastart3/common/RoleType.java
@@ -1,5 +1,26 @@
 package com.example.ch04jpastart3.common;
 
 public enum RoleType {
-    ADMIN,USER;
+    ADMIN("관리자"),USER("일반 사용자");
+
+    private String s;
+
+    RoleType(String s) {
+        this.s = s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    @Override
+    public String toString() {
+        return "RoleType{" +
+                "s='" + s + '\'' +
+                '}';
+    }
 }
