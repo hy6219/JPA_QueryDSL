f36b8cc1e23c24195c5dd3f786733b5cbaa50ebe
JSJeong <hy6219@naver.com>
2022-01-12 09:39:07 +0900

    chore: connect

---
+++ ch04-jpa-start3/src/main/java/com/example/ch04jpastart3/common/RoleType.java
@@ -0,0 +1,5 @@
+package com.example.ch04jpastart3.common;
+
+public enum RoleType {
+    ADMIN,USER;
+}
---
+++ ch04-jpa-start3/src/main/java/com/example/ch04jpastart3/domain/entity/Member.java
@@ -0,0 +1,11 @@
+package com.example.ch04jpastart3.domain.entity;
+
+/**
+ * @author gs813
+ * (1) 회원 고유 식별자(PK)
+ * (2) 회원 이름
+ * (3) 회원 나이(null 값 허용)
+ * (4) 회원 구분(RoleType)
+ */
+public class Member {
+}
