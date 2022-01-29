d239be3e5ae31a60a7ca15dc1cac4a6bddef3199
JSJeong <hy6219@naver.com>
2022-01-29 21:22:58 +0900

    docs:다대일 단방향,양방향

---
+++ ch06-jpa-start6/src/main/java/com/example/ch06jpastart6/Ch06JpaStart6Application.java
@@ -0,0 +1,13 @@
+package com.example.ch06jpastart6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch06JpaStart6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch06JpaStart6Application.class, args);
+    }
+
+}
