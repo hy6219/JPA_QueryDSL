ab43dcf1a2c44051a20f484c27fea6dba3d2ec0f
JSJeong <hy6219@naver.com>
2022-02-13 15:56:45 +0900

    chore: 구현클래스마다 테이블전략- `@PrimaryJoinColumn 어노테이션 부적절, 삭제`

--- ch07-jpa-start3/src/main/java/com/example/ch07jpastart3/domain/entity/Book.java
+++ ch07-jpa-start3/src/main/java/com/example/ch07jpastart3/domain/entity/Book.java
@@ -3,7 +3,6 @@ package com.example.ch07jpastart3.domain.entity;
 import lombok.*;
 
 import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
 @Setter
@@ -11,7 +10,6 @@ import javax.persistence.PrimaryKeyJoinColumn;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@PrimaryKeyJoinColumn(name = "book_id")
 public class Book extends Item{
     private String author;
     private String isbn;
