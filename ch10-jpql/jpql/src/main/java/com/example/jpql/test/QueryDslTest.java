1553f46f95112f729f7d888c1343d7c3b41a62f3
JSJeong <hy6219@naver.com>
2022-09-04 19:39:53 +0900

    feat: JPQL - QueryDSL 기본

--- ch10-jpql/jpql/src/main/java/com/example/jpql/test/QueryDslTest.java
+++ ch10-jpql/jpql/src/main/java/com/example/jpql/test/QueryDslTest.java
@@ -1,12 +1,14 @@
 package com.example.jpql.test;
 
 import com.example.jpql.domain.Member;
+import com.example.jpql.domain.QMember;
 import com.querydsl.jpa.impl.JPAQuery;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class QueryDslTest {
     public static void main(String[] args) {
@@ -16,6 +18,13 @@ public class QueryDslTest {
         EntityTransaction tx = em.getTransaction();
 
         JPAQuery<Member> query = new JPAQuery<>(em);
+        QMember qMember = QMember.member;
 
+        //2. 쿼리, 결과 조회
+        List<Member> members = query.from(qMember)
+                .where(qMember.username.eq("테스트1"))
+                .fetch();
+
+        System.out.println("results : " + members);
     }
 }
