033d5d4d993238d027b34d647d3460dd53887848
JSJeong <hy6219@naver.com>
2022-09-04 19:31:51 +0900

    feat: JPQL - QueryDSL 설정 이슈 정정

--- ch10-jpql/jpql/src/main/java/com/example/jpql/test/QueryDslTest.java
+++ ch10-jpql/jpql/src/main/java/com/example/jpql/test/QueryDslTest.java
@@ -1,5 +1,8 @@
 package com.example.jpql.test;
 
+import com.example.jpql.domain.Member;
+import com.querydsl.jpa.impl.JPAQuery;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -12,6 +15,7 @@ public class QueryDslTest {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
 
+        JPAQuery<Member> query = new JPAQuery<>(em);
 
     }
 }
