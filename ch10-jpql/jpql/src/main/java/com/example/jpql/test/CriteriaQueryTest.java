934647d4d8f7de292bc90c8181487e1b6d8b9062
JSJeong <hy6219@naver.com>
2022-09-04 18:43:19 +0900

    feat: JPQL - criteria 쿼리

---
+++ ch10-jpql/jpql/src/main/java/com/example/jpql/test/CriteriaQueryTest.java
@@ -0,0 +1,30 @@
+package com.example.jpql.test;
+
+import com.example.jpql.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class CriteriaQueryTest {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+        //1. Criteria 사용 준비
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+        //2. 루트 클래스(조회를 시작할 클래스)를 만들기
+        Root<Member> m = query.from(Member.class);
+
+        //3. 쿼리 생성
+        //현재 엔티티에서 name 컬럼을 "username"으로 받고 있고, name 속성의 값이 x인 경우를 조회할 것이므로 아래와 같이 될 것
+        CriteriaQuery<Member> where = query.select(m).where(cb.equal(m.get("username"), "테스트1"));
+        List<Member> resultList = em.createQuery(where).getResultList();
+        System.out.println("result: " + resultList);
+    }
+}
